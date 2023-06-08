d5a8bf0b9f94526f3d7824d5a17bc81896f48dd9
raghavgupta24 <raghav.24@yahoo.com>
2023-06-08 12:29:16 +0530

    changes a line

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const pokemon = require('pokemon');
 // console.log(pokemon.all());
 // console.log(pokemon.getName(147));
 
-console.log(pokemon.getId('Raichu'))
+console.log(pokemon.getId('Raichu'));
+console.log(pokemon.getName(222));
