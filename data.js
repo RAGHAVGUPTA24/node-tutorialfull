476595ab35a53230947edcda6be3eda030d40ede
raghavgupta24 <raghav.24@yahoo.com>
2023-06-09 12:13:55 +0530

    day2 prac

---
+++ data.js
@@ -0,0 +1,4 @@
+const data = [{ name: 'rghav', emial: 'test@sd.com' }, { name: 'rghasv', emial: 'test@sd.com', class: 34 },
+{ name: 'shiv', emial: 'tet@sd.com' }, { name: 'rghav', emial: 'test@ddsd.com' },
+{ name: 'ram', emial: 't@sd.com' }]
+module.exports = data;
